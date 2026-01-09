cc92df5fe515dde1fc4000ca9070a31bcda38cea
Samtechnerd <sampatton176@gmail.com>
2026-01-09 15:57:09 +0000

    Update const.py

--- custom_components/dublinbusbysam/const.py
+++ custom_components/dublinbusbysam/const.py
@@ -2,5 +2,6 @@
 
 DOMAIN = "dublinbusbysam"
 CONF_STOP_ID = "stop_id"
-CONF_FILTER_ROUTES = "filter_routes"  # New constant
+CONF_FILTER_ROUTES = "filter_routes"
+CONF_FRIENDLY_NAME = "friendly_name"  # New constant
 BASE_URL = "https://busdepart.sampatton176.workers.dev/?stopId={}"
